2eef295f91280af9e24b5859bc16f1ebfd708832
Ivy <ivy7661@gmail.com>
2024-08-10 23:37:55 +0800

    personal website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PersonalWebsiteComponent } from './page/personal-website/personal-website.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path:'personalWebsite',
+    component:PersonalWebsiteComponent
+  }
+];
---
+++ src/app/page/personal-website/personal-website.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-website',
+  standalone: true,
+  imports: [],
+  templateUrl: './personal-website.component.html',
+  styleUrl: './personal-website.component.scss'
+})
+export class PersonalWebsiteComponent {
+
+}
